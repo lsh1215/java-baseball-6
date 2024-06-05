562d74c2b2a9fde0ddc2a22a1ba7d7d730497e2b
sanghun <vitash1215@gmail.com>
2024-06-05 19:16:17 +0900

    feat : 힌트 생성

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -12,4 +12,26 @@ public class Computer {
         /*이후 랜덤으로 숫자 생성하는 클래스 작성*/
     }
 
+    public HintResult generateHintResult(List<Integer> givenNumbers) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == givenNumbers.get(i)) {
+                strike++;
+            }
+            ball += isBallIndex(i, givenNumbers);
+        }
+        return new HintResult(strike, ball);
+    }
+
+    private int isBallIndex(int i, List<Integer> givenNumbers) {
+        for (int j = 0; j < givenNumbers.size(); j++) {
+            if (i != j && numbers.get(i) == givenNumbers.get(j)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
 }
+
