ea4e5e476d04cc1341f9412f3cf114b624637109
sanghun <vitash1215@gmail.com>
2024-06-05 12:24:03 +0900

    feat : 콘솔 입출력 기능을 할 class 작성

--- src/main/java/baseball/view/ConsoleReader.java
+++ src/main/java/baseball/view/ConsoleReader.java
@@ -1,1 +1,24 @@
-입
+package baseball.view;
+
+import baseball.global.exception.BlankInputException;
+import camp.nextstep.edu.missionutils.Console;
+
+public final class ConsoleReader {
+    public static String enterMessage() {
+        return Validator.validate(Console.readLine());
+    }
+
+    private static class Validator {
+        public static String validate(String message) {
+            validateBlankInput(message);
+            return message;
+        }
+
+        private static void validateBlankInput(String message) {
+            if (message.isBlank()) {
+                throw new BlankInputException("Input cannot be blank");
+                /*예외처리 메시지 필요*/
+            }
+        }
+    }
+}
