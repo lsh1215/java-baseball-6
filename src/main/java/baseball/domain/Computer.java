b35f34eb883d6be9c72d192a304461e4bbb2a7b8
sanghun <vitash1215@gmail.com>
2024-06-05 12:40:44 +0900

    feat : 게임을 시작 기능 구현(Controller, Domain)

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> numbers;
+
+    public Computer() {
+        this.numbers = RandomNumberGenerator.generateRandomNumber();
+        /*이후 랜덤으로 숫자 생성하는 클래스 작성*/
+    }
+
+}
